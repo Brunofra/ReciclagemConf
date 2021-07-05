6ede68a081bec64420f889e9ad60850b184efedd
Bruno Alves <brunoalvesfra@gmail.com>
2021-07-05 10:19:33 -0300

    iniciando a aplicacao

---
+++ src/main/java/com/br/ReciclagemConf/ReciclagemConfApplication.java
@@ -0,0 +1,13 @@
+package com.br.ReciclagemConf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclagemConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReciclagemConfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ReciclagemConf/ReciclagemConfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ReciclagemConf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclagemConfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
